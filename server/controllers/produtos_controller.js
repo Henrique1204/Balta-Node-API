556f7ec0b8299a4039abe47ef6ee4ccc37c453a1
Paulo <pauloh16.pdr@gmail.com>
2021-03-20 10:41:27 -0300

    Implementar buscas de produtos

--- server/controllers/produtos_controller.js
+++ server/controllers/produtos_controller.js
@@ -1,8 +1,58 @@
 const mongoose = require('mongoose');
 const Produto = mongoose.model('Produto');
 
-exports.get = (req, res) => {
-    res.status(200).send({ titulo: 'Produtos' });
+exports.get = async (req, res) => {
+    try {
+        const dados = await Produto.find({ ativo: true }, 'titulo slug preco');
+
+        return res.status(200).send(dados);
+    } catch({ message }) {
+        console.log(message);
+        return res.status(500).send({ status: 'Falha', mensagem: 'Erro no servidor.' });
+    }
+};
+
+exports.getBySlug = async (req, res) => {
+    try {
+        const slug = req.params.slug;
+
+        const dados = await Produto.findOne({
+            slug,
+            ativo: true
+        }, 'titulo slug preco descricao tags');
+
+        return res.status(200).send(dados);
+    } catch({ message }) {
+        console.log(message);
+        return res.status(500).send({ status: 'Falha', mensagem: 'Erro no servidor.' });
+    }
+};
+
+exports.getById = async (req, res) => {
+    try {
+        const dados = await Produto.findById(req.params.id);
+
+        return res.status(200).send(dados);
+    } catch({ message }) {
+        console.log(message);
+        return res.status(500).send({ status: 'Falha', mensagem: 'Erro no servidor.' });
+    }
+};
+
+exports.getByTag = async (req, res) => {
+    try {
+        const tag = req.params.tag;
+
+        const dados = await Produto.findOne({
+            tag,
+            ativo: true
+        }, 'titulo slug preco descricao tags');
+
+        return res.status(200).send(dados);
+    } catch({ message }) {
+        console.log(message);
+        return res.status(500).send({ status: 'Falha', mensagem: 'Erro no servidor.' });
+    }
 };
 
 exports.post = async (req, res) => {
--- server/rotas/rotaProdutos.js
+++ server/rotas/rotaProdutos.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const controller = require('../controllers/produtos_controller.js');
 
 router.get('/', controller.get);
+router.get('/:slug', controller.getBySlug);
+router.get('/admin/:id', controller.getById);
+router.get('/tags/:tags', controller.getByTag);
 router.post('/', controller.post);
 router.put('/:id', controller.put);
 router.delete('/:id', controller.delete);
