7f6eaf021423072dafa0dcf404b75527105ab481
Paulo <pauloh16.pdr@gmail.com>
2021-03-20 08:24:31 -0300

    Adicionar mongoose ao projeto

--- server/index.js
+++ server/index.js
@@ -6,12 +6,23 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// Banco de dados.
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1/', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+});
+
 // Rota de teste.
 const rotaTeste = require('./rotas/rotaTeste.js');
 app.use('/', rotaTeste);
+
 // Rota de produtos.
 const rotaProdutos = require('./rotas/rotaProdutos.js');
 app.use('/produtos', rotaProdutos);
 
+// Criando servidor.
 const server = require('./serverConfig.js');
 server(app);
